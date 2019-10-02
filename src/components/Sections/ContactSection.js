f379e980d8a96f0c29e8226b62a28ff6ff5d4955
bram sikkens <bram.sikkens@student.ehb.be>
2019-10-02 16:21:27 +0200

    contact + image darker

--- src/components/Sections/ContactSection.js
+++ src/components/Sections/ContactSection.js
@@ -1,40 +1,39 @@
-import React, { useState } from 'react';
-import SectionHeader from "../SectionHeader";
-import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/styles';
-import axios from 'axios';
-
+import React, { useState } from "react"
+import SectionHeader from "../SectionHeader"
+import TextField from "@material-ui/core/TextField"
+import { withStyles } from "@material-ui/styles"
+import axios from "axios"
 
 const styles = {
   input: {
     color: "white",
-    '& .MuiFormLabel-root': {
-      color: '#707070',
+    "& .MuiFormLabel-root": {
+      color: "#707070",
     },
-    '& .MuiInputBase-root': {
-      color: 'white',
+    "& .MuiInputBase-root": {
+      color: "white",
     },
 
-    '& label.Mui-focused': {
-      color: '#707070',
-    },'& .MuiInput-underline:after': {
-      borderBottomColor: 'yellow',
-    },'& .MuiInput-underline:before': {
-      borderBottomColor: '#707070',
-    }, '&.Mui-focused fieldset': {
-      borderColor: '#707070',
+    "& label.Mui-focused": {
+      color: "#707070",
     },
-  }
-};
-
-
-
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "yellow",
+    },
+    "& .MuiInput-underline:before": {
+      borderBottomColor: "#707070",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#707070",
+    },
+  },
+}
 
 const ContactSection = props => {
-
-  
-  var name,email,message = "";
-  const { classes } = props;
+  var name,
+    email,
+    message = ""
+  const { classes } = props
 
   return (
     <div className="sectionContainer">
@@ -43,79 +42,89 @@ const ContactSection = props => {
         subDescription="Give us a short description of your business challenge, and we’ll arrange a meeting with our top business architect in your industry."
       />
       <form>
-        <div style={{
-          display:'flex',
-          width:"75%",
-          justifyContent:'space-between'
-        }}>
-        <div style={{
-          borderRight: "1px solid white",
-          paddingRight: "3%"
-        }}>
-        <TextField
-        fullWidth
-        id="name"
-        label="Name"
-        value={name}
-        margin="normal"
-        className={classes.input}
-        onChange={(e) => {
-          name = e.target.value}}
-
-      />
-        <TextField
-        fullWidth
-        id="email"
-        label="Email"
-        value={email}
-        name="email"
-        margin="normal"
-        className={classes.input}
-        onChange={(e) => {
-          email = e.target.value}}
-       
-      />
+        <div
+          style={{
+            display: "flex",
+            width: "75%",
+            justifyContent: "space-between",
+          }}
+        >
+          <div
+            style={{
+              borderRight: "1px solid white",
+              paddingRight: "3%",
+            }}
+          >
+            <TextField
+              fullWidth
+              id="name"
+              label="Name"
+              value={name}
+              margin="normal"
+              className={classes.input}
+              onChange={e => {
+                name = e.target.value
+              }}
+            />
+            <TextField
+              fullWidth
+              id="email"
+              label="Email"
+              value={email}
+              name="email"
+              margin="normal"
+              className={classes.input}
+              onChange={e => {
+                email = e.target.value
+              }}
+            />
+          </div>
+          <div
+            style={{
+              width: "60%",
+              paddingLeft: "5%",
+            }}
+          >
+            <TextField
+              placeholder="Write Text here..."
+              label="Message"
+              multiline={true}
+              name="message"
+              fullWidth
+              rowsMax={8}
+              className={classes.input}
+              onChange={e => {
+                message = e.target.value
+              }}
+            />
+          </div>
         </div>
-        <div style={{
-            width:"60%",
-            paddingLeft:"5%"
-        }}>
-        <TextField
-          placeholder="Write Text here..."
-          label="Message"
-          multiline={true}
-          name="message"
-          fullWidth
-          rowsMax={8}
-  className={classes.input}
-          onChange={(e) => {
-            message = e.target.value}}
-/>
-        </div>
 
-        </div>
-       
-      
-        <input className="button" style={{width:"25%", marginTop:"2%"}} type="submit" name="submit" value="SEND" onClick={(e)=>{
-          e.preventDefault();
-   
-          alert(name)
-          axios({
-            method:'post',
-            url:'http://nimium-env.ukqcmidxpz.eu-central-1.elasticbeanstalk.com/wp-json/rcdb/v1/messages',
-            data:{
-              fromName: name,
-              fromEmail: email,
-              message:message
-            }
-          })
+        <button
+          className="button"
+          style={{ width: "25%", marginTop: "2%" }}
+          type="submit"
+          name="submit"
+          onClick={e => {
+            e.preventDefault()
 
-          alert("send")
-
-        }} />
+            axios({
+              method: "post",
+              url:
+                "http://nimium-env.ukqcmidxpz.eu-central-1.elasticbeanstalk.com/wp-json/rcdb/v1/messages",
+              data: {
+                fromName: name,
+                fromEmail: email,
+                message: message,
+              },
+            })
+          }}
+        >
+          SEND
+        </button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default withStyles(styles)(ContactSection);
+export default withStyles(styles)(ContactSection)
--- src/components/UseCaseCard/UseCaseCard.js
+++ src/components/UseCaseCard/UseCaseCard.js
@@ -1,34 +1,30 @@
-import React from "react";
-import './UseCaseCard.css';
-import { Link } from "gatsby";
-import { Textfit } from 'react-textfit';
+import React from "react"
+import "./UseCaseCard.css"
+import { Link } from "gatsby"
+import { Textfit } from "react-textfit"
 
 const UseCaseCard = props => {
-
- const { previousCase, nextCase } = props;
+  const { previousCase, nextCase } = props
 
   return (
     <div className="case">
-      <div className="caseHeader">
-    
-        <h3>{props.case.acf.name}</h3>
+      <Link>
+        <div className="caseHeader">
+          <h3>{props.case.acf.name}</h3>
 
-        <p>{props.case.acf.function}</p>
+          <p>{props.case.acf.function}</p>
 
-        <img src={props.case.acf.main_image.source_url}></img>
-      </div>
+          <img src={props.case.acf.main_image.source_url}></img>
+        </div>
+      </Link>
+
       <div className="caseDescription">
-      
-      <Textfit mode="multi" >
-      {props.case.acf.summary}
-      </Textfit>
-     
-   
-      <Link
-      to={"/usecases/" +props.case.wordpress_id}>Read More</Link>  
+        <Textfit mode="multi">{props.case.acf.summary}</Textfit>
+
+        <Link to={"/usecases/" + props.case.wordpress_id}>Read More</Link>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default UseCaseCard;
+export default UseCaseCard
